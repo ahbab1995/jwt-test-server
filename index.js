d1910d75e776a7d8d3653fd031c7c7d11fa4c032
ahbab1995 <islamahbab1996@yahoo.com>
2023-06-04 02:09:44 +0600

    completing all authentication related verification

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const express = require('express')
-const app = express()
-var cors = require('cors')
-require('dotenv').config()
-const port = 5000
+const express = require("express");
+const app = express();
+var cors = require("cors");
+require("dotenv").config();
+const port = 5000;
 
-app.use(cors())
+// middleware
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/login", (req, res) => {
+    const user = req.body
+    console.log(user)
+  res.send({ success: true });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
